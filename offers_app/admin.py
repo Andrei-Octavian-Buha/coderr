9555a2513d2c753676408c846d2b989f8196543e
Andy <buha2595@gmail.com>
2026-05-04 11:35:28 +0200

    Offer model added

--- offers_app/admin.py
+++ offers_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Offer, OfferDetail
 
-# Register your models here.
+class OfferDetailInline(admin.TabularInline): # Aceasta este magia
+    model = OfferDetail
+    extra = 3 # Îți va afișa automat 3 rânduri goale pentru cele 3 pachete
+    max_num = 3 # Limitează la maxim 3 detalii per ofertă
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    inlines = [OfferDetailInline]
--- offers_app/models.py
+++ offers_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Offer(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.FileField(upload_to='offers/', blank=True, null=True)
+    description = models.TextField(max_length=2000)
+
+class OfferDetail(models.Model):
+    class OfferType(models.TextChoices):
+        STANDARD = "standard","Standard"
+        BASIC = "basic","Basic"
+        PREMIUM = "premium","Premium"
+        
+    offer = models.ForeignKey(Offer, on_delete=models.CASCADE,related_name='details')
+    title = models.CharField(max_length=255)
+    revisions = models.PositiveIntegerField(default=1)
+    delivery_time_in_days = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    features = models.JSONField(default=list)
+    offer_type = models.CharField(max_length=20, choices=OfferType.choices,default=OfferType.STANDARD)
