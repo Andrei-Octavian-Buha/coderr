f15825d66fcec5a6bdf9bac800cc35501e950397
Andy <buha2595@gmail.com>
2026-05-11 15:43:44 +0200

    filters to offerview added

---
+++ offers_app/api/filters.py
@@ -0,0 +1,9 @@
+from django_filters import rest_framework as filters
+from offers_app.models import Offer
+class OfferFilter(filters.FilterSet):
+    creator_id = filters.NumberFilter(field_name='user_id')
+    min_price =	filters.NumberFilter(field_name='details__price',lookup_expr='gte')
+    max_delivery_time = filters.NumberFilter(field_name='details__delivery_time_in_days',lookup_expr='lte')
+    class Meta:
+        model = Offer
+        fields = ['creator_id','min_price','max_delivery_time']
--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,12 +1,16 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import OfferSerializer, OfferDetailSerializer, OfferListSerializer
 from offers_app.models import Offer, OfferDetail
 from .permissions import IsBusinessUserOrReadOnly
+from .filters import OfferFilter
 
 class OfferView(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
     permission_classes = [IsAuthenticated, IsBusinessUserOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = OfferFilter
 
     def get_serializer_class(self):
         if self.action == 'list':
