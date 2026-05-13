4aaeab6dc9c431e0a19e5fb26aa6c466f183af1b
Andy <buha2595@gmail.com>
2026-05-13 13:13:10 +0200

    feat(profile_app):add create user from admin panel

--- profile_app/admin.py
+++ profile_app/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from profile_app.models import UserProfile
-
-# Register your models here.
+from rest_framework.authtoken.models import Token
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
@@ -14,9 +13,32 @@ class UserProfileInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (UserProfileInline, )
     list_display = ('id','username', 'email', 'first_name', 'last_name', 'is_staff', 'get_type')
+    readonly_fields = ('display_token',)
+
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name'),
+        }),
+    )
+    
     def get_type(self, instance):
         return instance.profile.type
     get_type.short_description = 'Profile Type'
+
+    def display_token(self, instance):
+        # Încercăm să găsim tokenul pentru acest user
+        try:
+            token = Token.objects.get(user=instance)
+            return token.key
+        except Token.DoesNotExist:
+            return "Niciun token generat"
+    
+    display_token.short_description = 'Auth Token'
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('User Token', {'fields': ('display_token',)}),
+    )
 
 
 admin.site.unregister(User)
--- profile_app/models.py
+++ profile_app/models.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save
 from django.db import models
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.conf import settings
+from rest_framework.authtoken.models import Token
 
 # Create your models here.
 
@@ -19,11 +21,14 @@ class UserProfile(models.Model):
     type = models.CharField(max_length=20, choices=TypeChoices.choices)
     created_at = models.DateTimeField(auto_now_add=True)
 
-@receiver(post_save, sender=User)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if not hasattr(instance, 'profile'):
         UserProfile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
