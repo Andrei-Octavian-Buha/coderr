f2503d1f3d3c39a8536aadd262b0a7f3c6e83e78
Andy <buha2595@gmail.com>
2026-05-14 21:13:52 +0200

    fix(profile_app):remove from models signals

--- profile_app/models.py
+++ profile_app/models.py
@@ -22,13 +22,5 @@ class UserProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_user_profile(sender, instance, created, **kwargs):
-    if not hasattr(instance, 'profile'):
-        UserProfile.objects.create(user=instance)
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
 
