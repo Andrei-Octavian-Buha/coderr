bc196ea0f11fd3b5c6e224cea0988887e658f692
Andrei Buha <119259186+Andrei-Octavian-Buha@users.noreply.github.com>
2026-07-13 12:42:04 +0200

    Set DEBUG to False for production readiness

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = 'django-insecure-%nw-k9=(2)ic^+!0trun+f=!tec5$zdcp+-0xgmz^und_sd2dz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'coderr.andreibuha.com',
