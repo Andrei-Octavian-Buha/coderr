2755f161fac8e97664cc072ea172c936c2fa8242
Andrei Buha <119259186+Andrei-Octavian-Buha@users.noreply.github.com>
2026-07-13 16:14:00 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -163,14 +163,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 6
 }
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'frontend'], # <--- Aici îi spui să caute în folderul tău
-        'APP_DIRS': True,
-        # ... restul setărilor
-    },
-]
 CORS_ALLOWED_ORIGINS = [
     "https://coderr.andreibuha.com",
     "http://127.0.0.1:5500", 
