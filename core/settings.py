923211a1d62ed331e4c476554eb246d281ce95ad
Andrei Buha <119259186+Andrei-Octavian-Buha@users.noreply.github.com>
2026-07-13 15:11:52 +0200

    Add STATICFILES_DIRS for frontend assets

--- core/settings.py
+++ core/settings.py
@@ -19,9 +19,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend',
+]
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
