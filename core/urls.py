ece2ae1a7fbb30d9ab6c918ea0de154d118ad707
Andrei Buha <119259186+Andrei-Octavian-Buha@users.noreply.github.com>
2026-07-13 15:56:15 +0200

    Import views from local module in urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
+from . import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
