ef7385c88de789889242db3e7955c92a59044f8c
Andrei Buha <119259186+Andrei-Octavian-Buha@users.noreply.github.com>
2026-07-13 15:57:53 +0200

    Remove base_info_view URL from core URLs
    Remove base_info_view URL from urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -26,7 +25,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('api/base-info/', views.base_info_view, name='base-info'),
     path('admin/', admin.site.urls),
     # path('api-token-auth/', views.obtain_auth_token),
     path('api/token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
