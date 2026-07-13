316bbde047320df436ed53439f1c1d7f6e6ec749
Andrei Buha <119259186+Andrei-Octavian-Buha@users.noreply.github.com>
2026-07-13 12:31:25 +0200

    Modify static file settings and add JWT authentication
    Updated static file settings and added REST framework authentication.

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 from datetime import timedelta
 import os
 
+# ... restul setărilor ...
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -127,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
